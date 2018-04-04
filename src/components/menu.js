f1f1ec664c4aef8422417ae36d6692928cca29ff
Pavol Rajzak <pavol.rajzak@itera.no>
2018-04-04 08:22:37 +0200

    fix: changed mobile scaling of menus

--- src/components/menu.js
+++ src/components/menu.js
@@ -33,8 +33,8 @@ Authors.propTypes = {
 
 const Menu = ({authors, categories}) => <div className="menu col-sm-3 col-12">
     <ul className="row">
-        <li className="col-4 col-sm-12"><Categories categories={categories}/></li>
-        <li className="col-4 col-sm-12"><Authors authors={authors}/></li>
+        <li className="col-6 col-sm-12"><Categories categories={categories}/></li>
+        <li className="col-6 col-sm-12"><Authors authors={authors}/></li>
     </ul>
     <Link to="/add-post">
         <button type="button" className="btn btn-outline-primary btn-block">
